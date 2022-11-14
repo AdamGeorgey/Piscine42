620831c68f36eabe122fecf3bf59f51a84b8a0df
Adam Georgey <adam.georgey.pro+numerique@gmail.com>
2022-11-14 22:48:42 +0100

    ft_print_comb2.c 99

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,52 @@
+#include    <unistd.h>
+
+void        ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void        ft_putcomb(int i)
+{
+    int     premier;
+    int     second;
+    
+    if (i > 9)
+    {
+        premier = (i / 10) + '0';
+        second = (i % 10) + '0';
+    }
+    else
+    {
+        premier = '0';
+        second = i + '0';
+    }
+    ft_putchar(premier);
+    ft_putchar(second);
+}
+
+void        ft_print_comb2(void)
+{
+    int     a;
+    int     b;
+    
+    a = 0;
+    b = 1;
+    while (a < 98)
+    {
+        while(b <= 99)
+        {
+            ft_putcomb(a);
+            ft_putchar(' ');
+            ft_putcomb(b);
+            write(1, ", ", 2);
+            b++;
+        }
+        b = ++a + 1;
+    }
+}
+
+int     main()
+{
+    ft_print_comb2();
+    return (0);
+}
