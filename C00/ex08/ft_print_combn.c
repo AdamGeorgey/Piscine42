e014b0bbaf6ca2cb96bfed0936a250a9976cbef3
Adam Georgey <adam.georgey.pro+numerique@gmail.com>
2022-11-15 00:19:06 +0100

    ft_print_combn.c 1

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -1,8 +1,27 @@
+#include    <unistd.h>
+
 void        ft_putchar(char c)
 {
     write(1, &c, 1);
 }
 
-void        ft_print_combn(int n)
+void        ft_putnbr(int nb)
 {
+    if (nb < 0)
+    {
+        nb = -nb;
+        ft_putchar('-');
+    }
+    if (nb > 9)
+    {
+        ft_putnbr(nb / 10);
+        ft_putnbr(nb % 10);
+    }
+    else
+        ft_putchar(nb + '0');
+}
+
+void        ft_print_combn(int nb)
+{
+    
 }
