46e63f9e41e895b4f59ee014284016a7f3caea9c
Adam Georgey <adam.georgey.pro+numerique@gmail.com>
2022-11-15 23:20:00 +0100

    ft_puttab() fonctionne

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -1,4 +1,5 @@
 #include    <unistd.h>
+#include    <stdio.h>
 
 void        ft_putchar(char c)
 {
@@ -21,9 +22,44 @@ void        ft_putnbr(int nb)
         ft_putchar(nb + '0');
 }
 
-void        ft_print_combn(int nb)
+void        ft_puttab(int tab[], int n)
 {
-    if (nb == 1)
-        ft_putchar('0');
+    int     i;
     
+    i = 0;
+    while (i < n - 1)
+    {
+        tab[i] = 0;
+        ft_putnbr(tab[i++]);
+    }
+    tab[i] = 1;
+    ft_putnbr(tab[i]);
+    tab[++i] = 0;
+    ft_putnbr(tab[i]);
+}
+
+void        ft_print_combn(int n)
+{
+    int     tab[n];
+    int     i;
+    int     j;
+    
+    i = 0;
+    j = n - 1;
+    while (tab[0] < 10 - n)
+    {
+        while (tab[j] < 10 - n - j)
+        {
+            ft_puttab(tab, n);
+            tab[j]++;
+        }
+    }
+}
+
+int         main()
+{
+    int     tab[9];
+    
+    ft_puttab(tab, 9);
+    return (0);
 }
